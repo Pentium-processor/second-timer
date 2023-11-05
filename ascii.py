688c51159371fffebf88ba1352b08bc16b827582
Analox <114465675+TomShir@users.noreply.github.com>
2023-11-05 12:39:20 +0000

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,37 +1,36 @@
-from itertools import permutations 
+from colorama import Fore 
 import time 
 import sys 
-from colorama import Fore 
-digit_length=1
+color_font_red=Fore.RED 
+color_font_green=Fore.GREEN 
 def error_msg(txt):
-    for n in txt:
-        sys.stderr.flush()
+    for ch in txt:
+        sys.stdout.flush()
         time.sleep(0.1)
-        sys.stderr.write(f'{Fore.RED}{n}')
+        sys.stdout.write(f'{color_font_red}{ch}')
     else:
-        reset_to_default_color=Fore.RESET 
-        print(f'{reset_to_default_color}')
+        print(f'{color_font_green}')
+        
 while True:
- try:
-  repeat=int(input('how many digits do you want?:'))
-  digits=[]
-  if repeat==1:
-      pass 
-  else:
-   for n in range(repeat):
-      digit=int(input('digit:'))
-      time.sleep(0.2)
-      strdigit=str(digit)
-      if len(strdigit)>1:
-          error_msg(txt='Error,digit should"nt be more than 1 character long\n')
-      else:
-          digits.append(strdigit)
-   else:
-      print('\n all orderings of your given digits:\n')
-      combinations=permutations(digits)
-      for num in combinations:
-          sys.stderr.flush()
-          time.sleep(0.5)
-          print(f'{"".join(num)}\n')
- except ValueError:
-     error_msg(txt='Error not a numeric value')
+    try:
+        print(color_font_green)
+        timer=int(input('Enter the number of seconds you want to count:'))
+        if timer>60 or timer<0:
+            error_msg('Error,we could not process your request, pls try again\n')
+        else:
+            if timer<10 and timer>1:
+                print(f'00:0{timer}')
+                time.sleep(1)
+            elif timer>10 and timer<60:
+                print(f'00:{timer}')
+                time.sleep(1)
+            while timer>0:
+                timer-=1 
+                if timer>=1 and timer<=9 or timer==0:
+                    print(f'00:0{timer}')
+                    time.sleep(1)
+                elif timer>=10:
+                    print(f'00:{timer}')
+                    time.sleep(1)
+    except ValueError:
+        error_msg('Error,not a  numerical value pls try again\n')
