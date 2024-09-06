a2b4a6b1d144eb162aa8ca47288dd8be0da5fe16
Analox <114465675+TomShir@users.noreply.github.com>
2024-09-06 15:58:45 +0100

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -19,18 +19,18 @@ while True:
             error_msg('Error,we could not process your request, pls try again\n')
         else:
             if timer<10 and timer>1:
-                print(f'00:0{timer}')
+                print(f'00:0{timer}',end='\r')
                 time.sleep(1)
             elif timer>10 and timer<60:
-                print(f'00:{timer}')
+                print(f'00:{timer}',end='\r')
                 time.sleep(1)
             while timer>0:
                 timer-=1 
                 if timer>=1 and timer<=9 or timer==0:
-                    print(f'00:0{timer}')
+                    print(f'00:0{timer}',end='\r')
                     time.sleep(1)
                 elif timer>=10:
-                    print(f'00:{timer}')
+                    print(f'00:{timer}',end='\r')
                     time.sleep(1)
     except ValueError:
         error_msg('Error,not a  numerical value pls try again\n')
