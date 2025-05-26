fd4fa57cb71f24cf935b4db607ce361c8c2c31b6
Sys Acrunis 1o1 <114465675+Pentium-processor@users.noreply.github.com>
2025-05-26 22:54:59 +0100

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,36 +1,36 @@
-from colorama import Fore 
+from rich.console import Console
+from rich.theme import Theme
 import time 
-import sys 
-color_font_red=Fore.RED 
-color_font_green=Fore.GREEN 
+import os
+custom_theme=Theme({"error":"bold red"})
+console=Console(theme=custom_theme)
 def error_msg(txt):
-    for ch in txt:
-        sys.stdout.flush()
+   for ch in txt:
+        console.print(ch,style="error",end='')
         time.sleep(0.1)
-        sys.stdout.write(f'{color_font_red}{ch}')
-    else:
-        print(f'{color_font_green}')
-        
-while True:
-    try:
-        print(color_font_green)
-        timer=int(input('Enter the number of seconds you want to count:'))
+try:
+     with console.screen():
+        timer=int(console.input("[green] [i]enter the number of seconds you would like to count:"))
         if timer>60 or timer<0:
             error_msg('Error,we could not process your request, pls try again\n')
         else:
             if timer<10 and timer>1:
-                print(f'00:0{timer}',end='\r')
+                console.print(f'00:0{timer}',justify="center")
                 time.sleep(1)
+                os.system("cls")
             elif timer>10 and timer<60:
-                print(f'00:{timer}',end='\r')
+                console.print(f'00:{timer}',justify="center")
                 time.sleep(1)
+                os.system("cls")
             while timer>0:
                 timer-=1 
                 if timer>=1 and timer<=9 or timer==0:
-                    print(f'00:0{timer}',end='\r')
+                    console.print(f'00:0{timer}',justify="center")
                     time.sleep(1)
+                    os.system("cls")
                 elif timer>=10:
-                    print(f'00:{timer}',end='\r')
-                    time.sleep(1)
-    except ValueError:
+                   console.print(f'00:{timer}',justify="center")
+                   time.sleep(1)
+                   os.system("cls")
+except ValueError:
         error_msg('Error,not a  numerical value pls try again\n')
